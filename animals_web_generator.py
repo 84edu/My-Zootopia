d91d6b187ad994709f3768b8f0acccb86580d5b4
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-01-29 18:05:35 +0100

    Insert animal data in HTML

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,31 +6,44 @@ def load_data(file_path):
     return json.load(handle)
 
 
+def load_html(file_path):
+    """Read the HTML file as text."""
+    with open(file_path, "r") as handle:
+        return handle.read()
+
+
 def get_animals_data(animals_data):
+    """A long string with all animals data"""
+    output = ""
     for animal in animals_data:
         name = animal.get("name")
         if name:
-            print(f"Name: {name}")
+            output += f"Name: {name}\n"
 
         characteristics = animal.get("characteristics", {})
         diet = characteristics.get("diet")
         if diet:
-            print(f"Diet: {diet}")
+           output += f"Diet: {diet}\n"
 
         location = characteristics.get("location")
         if location and len(location) > 0:
-            print(f"Location: {location}")
+            output += f"Location: {location[0]}\n"
 
-        type = characteristics.get("type")
-        if type:
-            print(f"Type: {type}")
+        type_info = characteristics.get("type")
+        if type_info:
+            output += f"Type: {type_info}\n"
 
-        print('-' * 30)
+    return output
 
 
-def main():
+def main(animals_string=None):
     animals_data = load_data('animals_data.json')
-    get_animals_data(animals_data)
+    template_content = load_html("animals_template.html")
+    animals_string = get_animals_data(animals_data)
+    new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", animals_string)
+    with open("animals.html", "w") as file:
+        file.write(new_html_content)
+
 
 if __name__ == "__main__":
     main()
